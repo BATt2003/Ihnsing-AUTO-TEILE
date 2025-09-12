ebf554c170cd2480c42b29d3e4a8316bdce2e59f
BATt2003 <etype7582816@gmail.com>
2025-09-12 17:25:12 +0200

    Delete js directory

--- js/autosize.js
+++
@@ -1,9 +0,0 @@
-
-// Проставляет width/height всем изображениям принудительно
-document.querySelectorAll('img:not([width]):not([height])').forEach(img => {
-  img.onload = function() {
-    this.setAttribute('width', this.naturalWidth);
-    this.setAttribute('height', this.naturalHeight);
-  };
-
-});
